054852fed19d0cae114be58eabf8e317e73c193d
Merce Casamitjana <casamitjana.agraz@gmail.com>
2021-03-21 07:50:03 +0100

    Add a testing the link controller

--- backend/src/controllers/linkController.test.js
+++ backend/src/controllers/linkController.test.js
@@ -2,40 +2,78 @@ const Link = require('../models/linkModel');
 const linkController = require('./linkController');
 
 jest.mock('../models/linkModel');
-let req;
-let res;
 
-beforeEach(() => {
-  req = {
-    body: {
-      folderId: 'FR359142021318',
-      name: 'Link name',
-      tag: 'tool'
-    }
-  };
+describe('Given a linkController function', () => {
+  describe('When createLink is invoked', () => {
+    let req;
+    let res;
 
-  res = {
-    send: jest.fn(),
-    status: jest.fn()
-  };
-});
-describe('When createFolder is invoked', () => {
-  test('Should call res.status', () => {
-    linkController.createLink(req, res);
-    expect(res.status).toHaveBeenCalledWith(200);
-  });
-});
+    beforeEach(() => {
+      req = {
+        body: {
+          folderId: 'FR359142021318',
+          name: 'Link name',
+          tag: 'tool'
+        }
+      };
 
-describe('When createFolder is invoked', () => {
-  test('Should call res.status 500', async () => {
-    const newFolder = new Link({});
-
-    newFolder.save.mockImplementationOnce(() => {
-      throw new Error();
+      res = {
+        send: jest.fn(),
+        status: jest.fn()
+      };
     });
 
-    await linkController.createLink(req, res);
+    test('Should call res.status', () => {
+      linkController.createLink(req, res);
 
-    expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.status).toHaveBeenCalledWith(200);
+    });
+
+    test('Should call res.status 500', async () => {
+      const newFolder = new Link({});
+
+      newFolder.save.mockImplementationOnce(() => {
+        throw new Error();
+      });
+
+      await linkController.createLink(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+    });
+  });
+
+  describe('When getAllLinks is invoked', () => {
+    let req;
+    let res;
+
+    beforeEach(() => {
+      req = {
+        params: {
+          groupId: 'Lk359142021318'
+        }
+      };
+
+      res = {
+        send: jest.fn(),
+        json: jest.fn(),
+        status: jest.fn()
+      };
+    });
+
+    test('Should call res.status 500 when return error', () => {
+      Link.find.mockImplementationOnce((query, callback) => callback(true));
+
+      linkController.getAllLinks(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+    });
+
+    test('Should call res.status 200', () => {
+      Link.find.mockImplementationOnce((query, callback) => callback(false));
+
+      linkController.getAllLinks(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+    });
   });
 });
