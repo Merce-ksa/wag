2a5da6e59aaf991e4d392227a9103e4ffdb93fcd
Merce Casamitjana <casamitjana.agraz@gmail.com>
2021-03-24 19:59:16 +0100

    fix add link view

--- frontend/src/redux/reducers/statusLinkReducer.js
+++ frontend/src/redux/reducers/statusLinkReducer.js
@@ -3,9 +3,9 @@ import linkActionTypes from '../actions/linkActionsTypes'
 export default function statusLinkReducer (state = [], actions) {
   switch (actions.type) {
     case linkActionTypes.CREATE_LINK:
-      return actions.statusLink
+      return actions.linksLastUpdated
     case linkActionTypes.CREATE_LINK_ERROR:
-      return actions.statusLink
+      return actions.linksLastUpdated
     default:
       return state
   }
